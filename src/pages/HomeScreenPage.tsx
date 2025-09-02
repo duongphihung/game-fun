778ee0cd32417620b39efa5619f853ecacbe641c
duongphihung <hungdarker113@gmail.com>
2025-09-02 19:15:11 +0700

    nothing

--- src/pages/HomeScreenPage.tsx
+++ src/pages/HomeScreenPage.tsx
@@ -5,7 +5,8 @@ const HomeScreenPage = () => {
     return (
         <div className="text-center">
             <h1 className="text-3xl font-bold">Xin chào năm nay sẽ chẳng có câu hỏi nào cả. Chỉ có 1 mini game nho nhỏ cho em chơi chill chill :3 </h1>
-            <h3>Luật chơi thì cũng dễ tìm các điểm liên tiếp là được. Mỗi điểm tương ứng với 500đ </h3>
+            <h3>Luật chơi thì cũng dễ tìm các điểm liên tiếp là được</h3>
+            <h3>Game này rất BÌNH THƯỜNG :3</h3>
             <Button>
                 <Link to="/game">Chơi ngay</Link>
             </Button>
